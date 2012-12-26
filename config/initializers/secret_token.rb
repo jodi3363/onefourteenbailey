81e245c217575c34c32efdcd37034cc5400602b4
Jodi Dias <jodi.dias@gmail.com>
2012-12-26 13:39:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onefourteenbailey::Application.config.secret_token = '1c525525572cfaeffe9bcd7d069ba81481acf7ac4abc231b6d76440f0d944f0745bcfa0937ac796058a9cae04933ff233731b88720e39cd6c5974a23d8b4ce26'
