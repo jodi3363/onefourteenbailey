0c460769c590183b5dd29e4495a0e6a35955c8fb
Jodi Dias <jodi.dias@gmail.com>
2012-12-29 16:04:25 -0500

    Add Google Tracking Code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+	def full_title(page_title)
+		base_title = "One Fourteen Bailey Interior Design"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+	def description(page_description)
+		base_description = "A new approach to interior design. Spaces that inspire - ideas that design happiness."
+		if page_description.empty?
+			base_description
+		else
+			"#{page_description}"
+		end		
+	end	
 end
