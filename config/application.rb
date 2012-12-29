ea4662e8941b7b0277d259c0b2e689f44fce54fd
Jodi Dias <jodi.dias@gmail.com>
2012-12-29 16:29:01 -0500

    test

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,7 @@ module Onefourteenbailey
     config.assets.enabled = true
 
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w( bootstrap.css )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
